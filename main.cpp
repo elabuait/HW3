6fbff410b8b8688d68438f989ee8f9f1ea58fb3f
elabuait <elabuait@icloud.com>
2025-03-25 12:03:16 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,7 +6,16 @@ using namespace std;
 
 int main()
 {
-
+/*************************************************************************
+** Author : Elias Abueita
+** Program : hw3, q1
+** Date Created : March 23, 2025
+** Date Last Modified : March 25, 2025
+** Usage : No command line arguments
+**
+** Problem:
+Demonstrates the use of inheritance using these Classes: PErson, Employee, Student, and GradStudent
+*************************************************************************/
     Person p1("Tony", "Stark", 1950, 21, 1);
     cout << p1.getFirstName() << " " << p1.getLastName() << " is born on this date: " << p1.getBirthdate() << endl;
 
@@ -18,6 +27,5 @@ int main()
 
     GradStudent g1("Elias", "Abueita", 35, 1997, 23, 06);
     cout<<"Is "<< g1.getFirstName() << " " << g1.getLastName() << " completed with his graduate studies? " << (g1.getCanGraduate() ? "Yes" : "No") << endl;
-
     return 0;
 }
