76f18c818c67133aa59b3ca966bf7e1ada918403
elabuait <elabuait@icloud.com>
2025-03-24 22:56:39 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,8 +9,5 @@ int main()
     Person p1("Tony", "Stark", 1950, 21, 1);
     cout << p1.getFirstName() << " born on: " << p1.getBirthdate() << endl;
 
-    Employee emp1("Alice", "Smith", 1985, 10, 20);
-    emp1.setPositionTitle("Software Engineer");
-    cout << emp1.getFirstName() << " works as: " << emp1.getPositionTitle() << endl;
     return 0;
 }
