7a80b6ff7d143717b2addd82894d2858c9a95828
elabuait <elabuait@icloud.com>
2025-03-24 21:47:33 -0400

    Add files via upload

--- Person.h
+++ Person.h
@@ -1,26 +1,23 @@
 #ifndef PERSON_H
 #define PERSON_H
 #include <string>
+using namespace std;
 
 class Person
 {
     public:
         Person();
-        Person(std::string first, std::string last, int year, int day, int month);
-        std::string getName();
-        std::string getBirthdate();
-
-    private:
-        std::string Fname;
-        std::string Lname;
+        Person(string first, string last, int year, int day, int month);
+        string getName();
+        string getBirthdate();
+        void setName(string first);
+        void setBirthdate(int year,int month, int day);
+private:
+        string Fname;
+        string Lname;
         int birthDay;
         int birthMonth;
         int birthYear;
-
-    protected:
-    /*You may move private members to protected if needed*/
-
-
 };
 class Employee : public Person {
     public:
@@ -28,11 +25,37 @@ class Employee : public Person {
 };
 class Student : public Person {
     public:
+    int getNumCredits();
+    void setNumCredits(int credits);
     private:
+    int num_credits;
 };
 class GradStudent : public Student {
     public:
     private:
 };
 
+
+
+string Person::getName() {
+    return Fname;
+}
+string Person::getBirthdate() {
+return to_string(birthDay) + to_string(birthMonth) + to_string(birthYear);
+}
+
+int Student::getNumCredits() {
+    return num_credits;
+};
+void Person::setName(string first) {
+    Fname = first;
+}
+void Person::setBirthdate(int year,int month, int day) {
+    birthYear = year;
+    birthMonth = month;
+    birthDay = day;
+}
+void Student::setNumCredits(int credits) {
+    num_credits = credits;
+}
 #endif // PERSON_H
